b141912cc39034ae1a2761691612699195bc2eed
Dima <fgjd.gh50@gmail.com>
2024-01-10 19:44:29 +0200

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
-import { defineConfig } from 'vite';
+
 module.exports = defineConfig({
-  base: '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
